aea9a5bf5a412d621f77e8ecc8ab0b8a784b2eaf
andriawan24 <fawaznaufal23@gmail.com>
2025-07-26 18:26:17 +0700

    feature: Set default media type to JSON

--- src/main/java/id/andriawan/newsapidemo/config/WebConfig.java
+++ src/main/java/id/andriawan/newsapidemo/config/WebConfig.java
@@ -1,6 +1,8 @@
 package id.andriawan.newsapidemo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,5 +22,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
     }
 }
